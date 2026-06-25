d57ee2ee24b2985e3671ec56d4c351a73cb7dcfe
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-06-25 17:06:44 -0400

    Delete automation directory

--- automation/fluidd/index.ts
+++
@@ -1,9 +0,0 @@
-// src/addons/v4l-controls/index.ts
-import V4LPanel from "./V4LPanel.vue"
-
-export const v4lAddon = {
-  id: "v4l_controls",
-  title: "Camera Controls",
-  icon: "mdi-camera",
-  component: V4LPanel
-}
--- automation/moonraker/extensions/v4l_controls/__init__.py
+++
@@ -1,4 +0,0 @@
-# marks this as a Python package
-from .v4l_controls import V4LControlsExtension
-
-__all__ = ["V4LControlsExtension"]

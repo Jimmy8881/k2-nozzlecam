b881f986c6fa12969e53c6d4f069dabca0cc56d1
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-08-27 21:59:50 -0400

    Update install-jk2pf.sh
    added installation of udev rule

--- install-jk2pf.sh
+++ install-jk2pf.sh
@@ -4,14 +4,14 @@ set -e
 
 SCRIPT_DIR="$(readlink -f $(dirname $0))"
 
-# copy the 3docam.env file to /etc/ustreamer/3docam.env
-cp -f "${SCRIPT_DIR}/3docam.env" /etc/ustreamer/3docam.env
+# copy the nozzle_cam.env file to /etc/ustreamer/nozzle_cam.env
+cp -f "${SCRIPT_DIR}/nozzle_cam.env" /etc/ustreamer/nozzle_cam.env
 
 # Start the camera service immediately
-systemctl start ustreamer@3docam
+systemctl start ustreamer@nozzle_cam
 
 # Enable the service to start automatically on system boot
-systemctl enable ustreamer@3docam
+systemctl enable ustreamer@nozzle_cam
 
 # copy the 3dov4lctls.cfg to /mnt/UDISK/printer_data/config/3dov4lctrls.cfg
 cp -f "${SCRIPT_DIR}/3dov4lctls.cfg" /mnt/UDISK/printer_data/config/3dov4lctls.cfg
@@ -19,5 +19,9 @@ cp -f "${SCRIPT_DIR}/3dov4lctls.cfg" /mnt/UDISK/printer_data/config/3dov4lctls.c
 # add the macro 3dov4lctls.cfg into the printer.cfg file
 python "${SCRIPT_DIR}/ensure_included.py" \
     ~/printer_data/config/overrides.cfg 3dov4lctls.cfg
+
+# copy camera-assignment.rules to /etc/udev/rules.d/camera-assignment.rules
+cp -f "${SCRIPT_DIR}/camera-assignment.rules" /etc/udev/rules.d/camera-assignment.rules
+chmod 755 /etc/udev/rules.d/camera-assignment.rules
 
 echo "Installation complete. reboot klipper to load the new 3DO camera control macros."

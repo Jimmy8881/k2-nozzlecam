7f9ab906ececb812219ad00fd80e590cf478e2c0
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-08-28 17:05:36 -0400

    Updated uninstall-jk2pf.sh
    added the removal of the modified stockcam.env and restore the backup

--- uninstall-jk2pf.sh
+++ uninstall-jk2pf.sh
@@ -10,6 +10,11 @@ rm -f /mnt/UDISK/printer_data/config/v4lctls.cfg
 # remove the nozzle_cam.env file
 rm -f /etc/ustreamer/nozzle_cam.env
 
+# remove the installed file/symlink and restore original stockcam.env if backup exists
+if [ -e /etc/ustreamer/stockcam.bak ]; then
+    rm -f /etc/ustreamer/stockcam.env
+    mv /etc/ustreamer/stockcam.bak /etc/ustreamer/stockcam.env
+
 # remove the camera-assignment.rules file
 rm -f /etc/udev/rules.d/camera-assignment.rules
 

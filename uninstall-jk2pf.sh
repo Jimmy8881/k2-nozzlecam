17d5ba2e20a124ae0f29814078b713e1619a7643
Jimmy8881 <63641669+Jimmy8881@users.noreply.github.com>
2026-08-27 22:22:28 -0400

    Updated uninstall-jk2pf.sh
    adds removal of udev assignment rule

--- uninstall-jk2pf.sh
+++ uninstall-jk2pf.sh
@@ -7,8 +7,11 @@ SCRIPT_DIR="$(readlink -f $(dirname $0))"
 # remove the v4lctls.cfg file
 rm -f /mnt/UDISK/printer_data/config/v4lctls.cfg
 
-# remove the 3docam.env file
-rm -f /etc/ustreamer/3docam.env
+# remove the nozzle_cam.env file
+rm -f /etc/ustreamer/nozzle_cam.env
+
+# remove the camera-assignment.rules file
+rm -f /etc/udev/rules.d/camera-assignment.rules
 
 # remove the 3dov4lctls gcode macro line from overrides.cfg
 python "${SCRIPT_DIR}/ensure_included.py" \
